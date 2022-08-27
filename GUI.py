a152477d281d03384ca497ecf80784186fcbf4f6
RabbitRayman <rabbitrayman@yandex.ru>
2022-08-27 12:31:35 +0300

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -8,16 +8,18 @@ from tkinter import messagebox as mb
 def main():
     def fromkinorium2letterboxd():
         kinorium_path = str(txt_in_data.get())
+        csvReader = csv.reader(codecs.open(kinorium_path, 'rU', 'utf-16'), delimiter='\t')
+        letterboxd_rows = [["Title", "Year", "Rating10"]]
+        for row in csvReader:
+            if (row[4] == "Фильм" or row[4] == "Мультфильм") and (row[0] != ""):
+                if (row[3] != ""):
+                    letterboxd_rows.append([row[3], row[5], row[0]])
+                else:
+                    letterboxd_rows.append([row[2], row[5], row[0]])
         with open("letterboxd.csv", mode="w", encoding='utf-8') as w_file:
-            file_writer = csv.writer(w_file, delimiter=",", lineterminator="\n")
-            file_writer.writerow(["Title", "Year", "Rating10"])
-            csvReader = csv.reader(codecs.open(kinorium_path, 'rU', 'utf-16'), delimiter='\t')
-            for row in csvReader:
-                if (row[4] == "Фильм" or row[4] == "Мультфильм") and (row[0] != ""):
-                    if (row[3] != ""):
-                        file_writer.writerow([row[3], row[5], row[0]])
-                    else:
-                        file_writer.writerow([row[2], row[5], row[0]])
+            file_writer = csv.writer(w_file)
+            file_writer.writerows(letterboxd_rows)
+
 
     def start():
         fromkinorium2letterboxd()
