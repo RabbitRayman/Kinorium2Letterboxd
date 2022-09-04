930b01f18f701f743a1ce4b0fd44d3ae0c21b9d5
RabbitRayman <rabbitrayman@yandex.ru>
2022-09-04 12:03:51 +0300

    Update console_version.py

--- console_version.py
+++ console_version.py
@@ -6,7 +6,7 @@ with open("votes_for_letterboxed.csv", mode="w", encoding='utf-8') as w_file:
     file_writer.writerow(["Title", "Year", "Rating10"])
     csvReader = csv.reader(codecs.open('votes.csv', 'rU', 'utf-16'), delimiter='\t')
     for row in csvReader:
-        if (row[4] == "Фильм" or row[4] == "Мультфильм") and (row[0] != ""):
+        if (row[4] == "Фильм" or row[4] == "Мультфильм"):
             if (row[3] != ""):
                 file_writer.writerow([row[3], row[5], row[0]])
             else:
